3b6771c41e5fa7d05f861f8b6cdd97ac7a5cc819
Olena <alchimeraa@gmail.com>
2024-09-29 14:21:46 +0300

    js

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import { fetchImages } from './js/pixabay-api.js';
+import { renderImages, showLoader, hideLoader } from './js/render-functions.js';
+
+const searchForm = document.querySelector('.search-form');
+// const lightbox = new SimpleLightbox('.gallery a');
+
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const query = document.querySelector('input[name="query"]').value.trim();
+    if (!query) {
+        iziToast.error({
+            title: 'Error',
+            message: 'Search query cannot be empty.',
+        });
+        return;
+    }
+
+    showLoader();
+    fetchImages(query)
+        .then(images => {
+            renderImages(images);
+        })
+        .catch(error => {
+            console.error(error);
+            iziToast.error({
+                title: 'Error',
+                message:
+                    'An error occurred while fetching images. Please try again later.',
+            });
+        })
+        .finally(() => {
+            hideLoader();
+        });
+});
